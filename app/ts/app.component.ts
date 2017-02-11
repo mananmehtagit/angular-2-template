b61426700502c05f46afa3e333202093003fbabd
mananmehtagit <mananmehta64@gmail.com>
2017-02-10 20:47:28 -0500

    The NewBoston Tutorial 9 commit

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,11 +1,26 @@
 import {Component} from 'angular2/core';
 import {Config} from './config-service';
+import {Videos} from './videos';
+import {PlaylistComponents} from './playlist.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{ mainHeading }}</h1>'
+    templateUrl: 'app/ts/app.component.html',
+    directives: [PlaylistComponents]
 })
 
 export class AppComponent {
 	mainHeading = Config.MAIN_HEADING;
+	videos:Array<Videos>;
+	//Videos: Videos[];
+
+
+	constructor(){
+		this.videos = [
+			new Videos (1, "Zalima - Raees","hhdSyBHuI88"),
+			new Videos (2, "Enna Sona","ofTxceS4wLI"),
+			new Videos (3, "Ae Dil Hai Mushkil","vUCM_0evdQY")
+
+		]
+	}
 }
--- app/ts/playlist.component.ts
+++ app/ts/playlist.component.ts
@@ -1,9 +1,15 @@
 import {Component} from 'angular2/core';
+import {Videos} from './videos';
 
 @Component({
 	selector:"playlist",
-	templateUrl: 'app/ts/playlist.component.html'
+	templateUrl: 'app/ts/playlist.component.html',
+	inputs: ['videoAttributes']
 })
 
 export class PlaylistComponents{
+
+	onSelect(vid:Videos){
+		console.log(JSON.stringify(vid));
+	}
 }
