ab383a5ac888e70afd74dacb11f120242633e2f6
mananmehtagit <mananmehta64@gmail.com>
2017-02-10 19:39:56 -0500

    Session 1 commit

---
+++ app/ts/videos.ts
@@ -0,0 +1,12 @@
+export class Videos{
+
+	id:number;
+	title:string;
+	videoCode:string;
+
+	constructor(id:number,title:string,videoCode:string){
+		this.id=id;
+		this.title=title;
+		this.videoCode=videoCode;
+	}
+}
